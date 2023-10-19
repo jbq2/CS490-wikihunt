9295031d046e4f204e31e67392b2ec418d9badaa
Peter <pja4@njit.edu>
2023-10-19 07:54:14 -0400

    game components now hidden until start is pressed

--- src/shared-components/WikiGame.svelte
+++ src/shared-components/WikiGame.svelte
@@ -8,6 +8,7 @@
     let count: number = 0;
     let firstPage:string = "";
     let isWin = false;
+    let startCheck:boolean = false;
 
     let timerComponent: Timer;
 
@@ -192,33 +193,41 @@
     #wiki-page-container :global(figcaption) {
         width:20rem
     }
+
 </style>
 
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <main on:click={clickLink}>
-    {#if isWin}
-        <h1 id="win-message">You Win!</h1>
-        <h2 id="win-caption">You found "{ endPage }"</h2>
-        <h2 id="win-time">in { timerComponent.getTime() }</h2>
-    {/if}
-    <input type="text" bind:value={ currPage } placeholder="Enter Wikipedia page title" />
-    <button on:click={ fetchWikiPage }>Load Page</button>
-    <button on:click={ startGame }>Start Game</button>
-    <button on:click={ restartGame }>Restart Game</button>
-    <div 
-        id="main-container"
-        style="filter: blur({isWin ? '5px' : '0px'})"
-    >
-        <p> Wikipedia Articles Clicked: { count }</p> <!-- counter is at the bottom, not formated the best-->
-        <Timer bind:this={ timerComponent } />
-        
-        <div id="wiki-page-container">
-            {#if currPage}
-                <h1>{ currPage }</h1>
-            {/if}
-            {@html pageContent} <!-- loads content -->
+    {#if !startCheck}
+        <button id="start-button" on:click={() =>{ 
+        startCheck = true;
+        startGame();
+        }}>Start Game</button>
+    {:else}         
+        {#if isWin}
+            <h1 id="win-message">You Win!</h1>
+            <h2 id="win-caption">You found "{ endPage }"</h2>
+            <h2 id="win-time">in { timerComponent.getTime() }</h2>
+        {/if}
+        <input type="text" bind:value={ currPage } placeholder="Enter Wikipedia page title" />
+        <button on:click={ fetchWikiPage }>Load Page</button>
+        <button on:click={ startGame }>Start Game</button>
+        <button on:click={ restartGame }>Restart Game</button>
+        <div 
+            id="main-container"
+            style="filter: blur({isWin ? '5px' : '0px'})"
+        >
+            <p> Wikipedia Articles Clicked: { count }</p> <!-- counter is at the bottom, not formated the best-->
+            <Timer bind:this={ timerComponent } />
+            
+            <div id="wiki-page-container">
+                {#if currPage}
+                    <h1>{ currPage }</h1>
+                {/if}
+                {@html pageContent} <!-- loads content -->
+            </div>
         </div>
-    </div>
+    {/if}
 </main>

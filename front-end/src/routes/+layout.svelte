6901e19df8faff405828e15f627170e456ca1162
Peter <pja4@njit.edu>
2023-11-03 10:08:14 -0400

    redesigned home and header - nav bar beautified, logo replaced and linked to home

--- front-end/src/routes/+layout.svelte
+++ front-end/src/routes/+layout.svelte
@@ -1,19 +1,55 @@
-<nav>
-    <ul>
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/wiki">Wiki</a>
-        </li>
-        <li>
-            <a href="/leaderboard">Leaderboard</a>
-        </li>
-    </ul>
-</nav>
-
+<div id="header">
+    <a href="/">
+        <img class="logoimage" src={'src/lib/assets/wikilogo.png'} alt="Logo">
+        <!-- <h1 style="margin: 0 10px;">WikiHunt</h1> -->
+    </a>
+    <nav style="text-align: center">
+        <ul style="padding: 0 0 0 20px;" id="nav-bar">
+            <a id="left-link" href="/"><li style="left: 1.6rem" id="nav-bar-link1">Home</li></a>
+            <a href="/wiki"><li id="nav-bar-link2">Play</li></a>
+            <a id="right-link" href="/leaderboard"><li style="right: 1.6rem" id="nav-bar-link3">Leaderboard</li></a>
+        </ul>
+    </nav>
+</div>
 <slot />
 
 <style>
     @import '/public/global.css';
+
+    nav ul li {
+        border-radius: 0px;
+        border: 2px #dedede;
+        border-style: groove;
+        position: relative;
+        margin: 0;
+    }
+    nav ul li::before {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 4px;
+        background-color: green; 
+        opacity: 0;
+        transition: opacity 0.3s ease;
+    }
+
+    nav ul li:hover::before {
+        opacity: 1;
+    }
+
+    #nav-bar-link1, #nav-bar-link2, #nav-bar-link3 {
+        list-style-position: relative;
+        width: 5rem;
+    }   
+
+    #nav-bar-link1, #nav-bar-link2 {
+        text-indent: -1rem;
+    }
+
+    #nav-bar-link3 {
+        text-indent: -0.25rem;
+    }
+
 </style>

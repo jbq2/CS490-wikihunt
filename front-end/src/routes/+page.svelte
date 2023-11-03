39f6b1ea3df47c6e08cdbfa3da3a61c5be28de73
Peter <pja4@njit.edu>
2023-11-03 10:08:14 -0400

    redesigned home and header - nav bar beautified, logo replaced and linked to home

--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,8 +1,62 @@
 
-<h1>Welcome to WikiHunt</h1>
-
-
 <style>
+    .centered-container {
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        padding: 50px;
+    }
+
+    .page-content {
+        margin: 20px;
+        background-color: #ffffff;
+        border-radius: 10px;
+        padding: 20px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+
+    h1 {
+        color: #333;
+    }
+
+    p {
+        color: #666;
+        font-size: 16px;
+    }
+
+    ul {
+        color: #666;
+        list-style-type: square;
+        padding-left: 20px;
+    }
+
+    li {
+        margin: 10px 0;
+    }
+
+</style>
+
+<div class="centered-container">
+    <h1>Welcome to WikiHunt!</h1>
+    <div class="page-content">
+        <p>Welcome to WikiHunt, a Wikipedia game where the player must navigate from one randomly selected article to another pre-selected article.</p>
+        <h1>Rules</h1>
+        <ul>
+            <li>The user can only navigate by only clicking through links within the same article. No search bar allowed!</li>
+            <li>The user must do this with as little time as possible and with the least amount of clicks.</li>
+            <li>No backtracking!</li>
+        </ul>
+    </div>
+</div>
+
+
+
+<!-- <h1 style="text-align: center;">Welcome to WikiHunt!</h1> -->
+
+
+<!-- <style>
     .centered-container{
         text-align: center;
         display: flex;
@@ -18,10 +72,10 @@
 <div class="centered-container">
     <img class = "logoimage" src= {'src/lib/assets/wikilogo.png'} alt="Logo"  />
 </div>
+-->
 
-
-<p> 
-    Welcome to WikiHunt, a Wikipedia game where the player must navigate from one randomly selected article and to 
+<!-- <p> 
+    Welcome to WikiHunt, a Wikipedia game where the player must navigate from one randomly selected article and to
     another pre-selected article. </p>
 
 
@@ -41,6 +95,6 @@
 </li>
 
 
-</ul>
+</ul> -->
 
 

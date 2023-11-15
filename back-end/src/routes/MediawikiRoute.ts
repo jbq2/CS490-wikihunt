8348cb15e345033c036e2a38b0aa6d3a6d609ca1
Joshua Quizon <joshquizon@gmail.com>
2023-11-15 09:47:08 -0500

    get true end title before sending to front end

--- back-end/src/routes/MediawikiRoute.ts
+++ back-end/src/routes/MediawikiRoute.ts
@@ -3,6 +3,7 @@ import fetch from 'node-fetch';
 import { ArticleEnhancerUtil } from '../util/ArticleEnhancerUtil.js';
 import { JSDOM } from 'jsdom';
 import { mediaWikiUrlRoot, wordList } from '../constants/constants.js';
+import { ArticleContent } from '../constants/models.js';
 
 export const mediawikiRouter = Router();
 
@@ -14,33 +15,51 @@ mediawikiRouter.get('/startend', async (req, res) => {
         end = Math.floor(Math.random() * wordList.length);
     } 
 
+    let endTitle: string = wordList[end];
+    let apiUrl: string = `${mediaWikiUrlRoot}?action=parse&format=json&origin=*&page=${endTitle}&prop=text`;
+    let temp: ArticleContent = await getArticleContent(apiUrl, endTitle);
+    endTitle = temp.title;
+
     res.json({
         start: wordList[start],
-        end: wordList[end]
+        end: endTitle
     });
 });
 
 // GET endpoint to return enhanced/cleaned article html
 mediawikiRouter.get('/page/:title', async (req, res) => {
     let title: string = req.params['title']
-    let apiUrl: string = `${mediaWikiUrlRoot}?action=parse&format=json&origin=*&page=${title}&prop=text`;   
-    let doc: Document = await getHtmlDoc(apiUrl);
+    const apiUrl: string = `${mediaWikiUrlRoot}?action=parse&format=json&origin=*&page=${title}&prop=text`;   
+    const temp: ArticleContent = await getArticleContent(apiUrl, title);
 
-    while(doc.querySelector('div[class="redirectMsg"]')) {
-        title = String(doc.querySelector('a')?.getAttribute('title'));
-        apiUrl = `${mediaWikiUrlRoot}?action=parse&format=json&origin=*&page=${title}&prop=text`;
-        doc = await getHtmlDoc(apiUrl);
-    }
+    title = temp.title;
+    const html: string = ArticleEnhancerUtil.cleanArticle(temp.doc);
 
     res.json({
         title: title,
-        html: ArticleEnhancerUtil.cleanArticle(doc)
+        html: html
     });
 });
 
-async function getHtmlDoc(apiUrl: string): Promise<Document> {
+async function getArticleContent(apiUrl: string, queryTitle: string): Promise<ArticleContent> {
+    let doc: Document = await fetchHtmlDoc(apiUrl);
+    let title: string = queryTitle;
+    while(doc.querySelector('div[class="redirectMsg"]')) {
+        title = String(doc.querySelector('a')?.getAttribute('title'));
+        apiUrl = `${mediaWikiUrlRoot}?action=parse&format=json&origin=*&page=${title}&prop=text`;
+        doc = await fetchHtmlDoc(apiUrl);
+    }
+
+    return <ArticleContent>{
+        title: title,
+        doc: doc
+    };
+}
+
+async function fetchHtmlDoc(queryTitle: string): Promise<Document> {
+    const apiUrl = `${mediaWikiUrlRoot}?action=parse&format=json&origin=*&page=${queryTitle}&prop=text`;
     const data = await fetch(apiUrl)
         .then((response) => response.json())
         .then((data: any) => data && data.parse && data.parse.text ? data.parse.text['*'] : null);
-    return (new JSDOM(data)).window.document
+    return (new JSDOM(data)).window.document;
 }

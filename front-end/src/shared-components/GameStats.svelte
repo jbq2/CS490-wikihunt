aba434b7b52ed02978700ba0275b38319a1edb1f
Peter <pja4@njit.edu>
2023-11-25 17:25:15 -0500

    added all time best details, fixed 500 error code

--- front-end/src/shared-components/GameStats.svelte
+++ front-end/src/shared-components/GameStats.svelte
@@ -1,40 +1,42 @@
 <script lang="ts">
 	import Modal from './Modal.svelte';
-	import { today, readFromCookie, dailyStreakCookieName, lastPlayedCookieName, allTimeBestCookieName } from '$lib/CookieHelper';
+	import { today, readFromCookie, dailyCookieName, dailyStreakCookieName, lastPlayedCookieName, allTimeBestCookieName } from '$lib/CookieHelper';
 	import type { DateFormat, Stats } from "../constants/models";
+	import { onMount } from 'svelte';
+	import { tick } from 'svelte';
 
-	let showModal: boolean = false;
+	const fireEmoji: string = '🔥';
+	export let showModal: boolean = false;
 	let noCookies: boolean = false;
-	let allTimeBest: Stats | any | undefined = readFromCookie(allTimeBestCookieName);
+	let allTimeBest: Stats | any;
+	let allTimeBestString: string;
+	let cookieCheck: boolean = false;
 	let lastPlayed: DateFormat | any;
 	let streak: number;
-	const fireEmoji: string = '🔥';
-	if (!allTimeBest){
-		noCookies = true;
-		allTimeBest = 'No Records!';
-		lastPlayed = 'No Records!';
-		streak = 0;
-	}
-	else{
-		lastPlayed = readFromCookie(lastPlayedCookieName);
-		if (JSON.stringify(lastPlayed) === JSON.stringify(today))
-			lastPlayed = 'Today 😎';
-		else
-			lastPlayed = dateFormatter(lastPlayed);
-		streak = readFromCookie(dailyStreakCookieName).count;
-
-		// let date: string = dateFormatter(allTimeBest.date);
-		// let path: string = allTimeBest.goal['start'] + ' → ' + allTimeBest.goal['end'];
-		// let count: string = allTimeBest.clicks;
-		// let time: string = allTimeBest.playTime.minutes + ' minutes and ' + allTimeBest.playTime.seconds + ' seconds'
-		// allTimeBest = `
-		// \nDate: ${date}
-		// \nPath: ${path}
-		// \nCount: ${count}
-		// \nTime: ${time}
-		// `;
-		// console.log(allTimeBest)
-	}
+	let dailyGame: Stats | any;
+	onMount(() => {
+		allTimeBest = readFromCookie(allTimeBestCookieName);
+		if (allTimeBest)
+			cookieCheck = true;
+		
+		if (!allTimeBest){
+			allTimeBestString = "No Records!";
+			lastPlayed = 'No Records!';
+			streak = 0;
+		}
+		else{
+			noCookies = true;
+			lastPlayed = readFromCookie(lastPlayedCookieName);
+			if (JSON.stringify(lastPlayed) === JSON.stringify(today))
+				lastPlayed = 'Today 😎';
+			else
+				lastPlayed = dateFormatter(lastPlayed);
+			streak = readFromCookie(dailyStreakCookieName).count;
+			dailyGame = readFromCookie(dailyCookieName);
+			if (JSON.stringify(today) !== JSON.stringify(dailyGame.date))
+				dailyGame = undefined;
+		}
+	});
 
 	function dateFormatter(date: DateFormat): string  {
 		return `${date.month}/${date.day}/${date.year}`
@@ -52,19 +54,20 @@
 	<div style="list-style-type" class="grid">
 		<div id='last-and-streak'><b>Last Played</b>: {lastPlayed}</div>
 		<div id='last-and-streak'><b>Daily Streak</b>: {streak}{streak ? fireEmoji : ''}</div>
-		<!-- <div id='all-time-best'>
-			<b>All Time Best</b>:<br>{allTimeBest}
-		</div> -->
 		<div id='all-time-best'>
-			<b>All Time Best</b>:
-			{#if noCookies}
-				{allTimeBest}
+			<div><b>All Time Best</b>:
+			{#if !noCookies}
+				{' '+allTimeBestString}
 			{:else}
 				<div>Date: {dateFormatter(allTimeBest.date)}</div>
-				<div>Path: {allTimeBest.goal['start']} → {allTimeBest.goal['end']}</div>
-				<div>Count: {allTimeBest.clicks}</div>
+				<div>Goal: {allTimeBest.goal['start']} → {allTimeBest.goal['end']}</div>
+				<div>Clicks: {allTimeBest.clicks}</div>
 				<div>Time: {allTimeBest.playTime.minutes} minutes and {allTimeBest.playTime.seconds} seconds</div>
 			{/if}
 		</div>
+	</div>
+	<hr/>
+	<h2 id="daily-game-header">Daily Game Results</h2>
+	<div id="daily-game-body">
 	</div>
 </Modal>
